b1ca2701fbff4ef5a35c8d9c8aac51e387bedcaf
thejameshoward <jrhoward@utexas.edu>
2023-05-09 13:36:56 -0500

    Added bitmap gui program for converting premade bitmap images

--- README.md
+++ README.md
@@ -17,17 +17,18 @@
 
         python3 gui.py
 
-## Using as a bit-depth reducer
-Outputs a file in the same directory as the input file except with the correct number of bits.
+## Using as a bit-depth reducer and Encoder
+Run the bitmap_gui.py and select your image. You can reduce the number of colors by changing the "color depth" and pressing the enter key. This will round the colors in your image to the nearest color in the lower bitdepth space.
 
-        python3 colordepth.py all-colors-raw.png 2
+        python3 bitmap_gui.py
 
-
+16,777,216 colors
 ![input image](https://github.com/thejameshoward/PixelArt/blob/main/images/all-colors-raw.png?raw=true)
 
+4,096 colors
 ![resulting image](https://github.com/thejameshoward/PixelArt/blob/main/images/all-colors-raw-converted.png?raw=true)
 
-## Outputing encoding
+## Outputing encoding (Not recommended - use GUI)
 To convert an image into RGB representations and encode them as monomors, use the following code.
 
         python3 encoder.py MorganSmall-converted.png 4
--- bitmap_gui.py
+++ bitmap_gui.py
@@ -1,7 +1,8 @@
 
 import sys
 from pathlib import Path
-from PIL import Image, ImageEnhance, ImageTk
+from PIL import Image, ImageTk
+from pprint import pprint
 
 import tkinter as tk
 from tkinter import END,Label, filedialog as fd
@@ -24,11 +25,10 @@ class application:
         window_height: int = 900) -> None:
 
         self.root = tk.Tk()
-        self.root.title('Pixel Art Generator')
+        self.root.title('Bitmap Encoder')
         self.raw_image = None
         self.raw_scaled = None
         self.im1 = None
-        self.im2 = None
 
         # Set x and y coordinates for the Tk root window
         ws = self.root.winfo_screenwidth() # width of the screen
@@ -43,52 +43,19 @@ class application:
         button_frame = tk.Frame(self.root)
         button_frame.grid(row = 0, column = 0)
         Button(button_frame, text='Exit Application', width=11, command=self.ExitApplication).grid(row=0, column=0)
-        Button(button_frame, text='Select raw image', fg='#000000', width=11, command=self.PickImageFile, bg='brown').grid(row=0, column=1)
-        Button(button_frame, text='96-wellplate', fg='#000000', width=11, command=lambda: self.set_size(None, size=(8,12)), bg='brown').grid(row=0, column=2)
-        Button(button_frame, text='384-wellplate', fg='#000000', width=11, command=lambda: self.set_size(None, size=(16,24)), bg='brown').grid(row=0, column=3)
+        Button(button_frame, text='Select bitmap', fg='#000000', width=11, command=self.PickImageFile, bg='brown').grid(row=0, column=1)
         Button(button_frame, text='PrintRGB', fg='#000000', width=11,command=self.PrintRGB, bg='brown').grid(row=1, column=0)
-        Button(button_frame, text='Save', fg='#000000', width=11,command=self.SaveImage, bg='brown').grid(row=1, column=1)
-        Button(button_frame, text='Export RGB', fg='#000000', width=11,command=self.ExportExcel, bg='brown').grid(row=1, column=2)
-    
+        Button(button_frame, text='Export RGB', fg='#000000', width=11,command=self.ExportExcel, bg='brown').grid(row=1, column=1)
+
         # Sliders
         slider_frame = tk.Frame(self.root)
         slider_frame.grid(row=1, column = 0, sticky=tk.W)
-        self.scale_x = tk.Scale(slider_frame, from_=1, to=150, resolution = 1, length=125 ,orient='horizontal', command=self.resize)
-        self.scale_x.grid(row = 0, column = 0, padx=15)
-        self.scale_x.set(8)
-        self.scale_y = tk.Scale(slider_frame, from_=1, to=150, resolution = 1, length=125, orient='horizontal', command=self.resize)
-        self.scale_y.grid(row = 0, column = 1, padx=15)
-        self.scale_y.set(12)
-        self.contrast_slider = tk.Scale(slider_frame, from_=0, to=5, orient='horizontal', resolution=0.1, command=self.UpdateImageEnhancements)
-        self.contrast_slider.set(1)
-        self.color_slider = tk.Scale(slider_frame, from_=0, to=5, orient='horizontal', resolution=0.1, command=self.UpdateImageEnhancements)
-        self.color_slider.set(1)
-
-        # Set slider position and labels
-        self.scale_x.grid(row=1, column=0)
-        Label(slider_frame, text='Resample width').grid(row=2, column=0, padx=15)
-
-        self.scale_y.grid(row=1, column=1)
-        Label(slider_frame, text='Resample height').grid(row=2, column=1, padx=15)
-
-
-        # Saturation and contrast slider
-        self.contrast_slider.grid(row=1, column=2)
-        Label(slider_frame, text='Contrast').grid(row=2, column=2)
-
-        self.color_slider.grid(row=1, column=3)
-        Label(slider_frame, text='Saturation').grid(row=2, column=3)
-
-        # Keep the current saturation and contrast stored so that we don't
-        # Update it if we aren't actively changing it
-        self.currentContrast = self.GetContrast(None)
-        self.currentSaturation = self.GetColor(None)
 
         # Entry boxes
         self.bases = tk.Entry(slider_frame, width=4)
         self.bases.grid(row=3, column=0)
         self.bases.insert(0, str(4))
-        Label(slider_frame, text='Encoding Base').grid(row=4, column=0)
+        Label(slider_frame, text='# Monomers').grid(row=4, column=0)
 
         bit_depth = tk.StringVar()
         bit_depth_entry = tk.Entry(slider_frame, width=4, textvariable=bit_depth)
@@ -109,10 +76,8 @@ class application:
         self.root.grid_columnconfigure(1, weight=5)
 
         self.left_im = Label(image_frame, image=self.im1)
-        self.right_im = Label(image_frame, image=self.im2)
 
         self.left_im.grid(row=0, column=0)
-        self.right_im.grid(row=0, column=1)
 
         self.root.mainloop()
 
@@ -123,6 +88,8 @@ class application:
 
         f = fd.asksaveasfilename(defaultextension='.xlsx')
 
+
+        img = self.image_to_save
         # Check if a file was selected
         if f == '':
             return 1
@@ -135,12 +102,9 @@ class application:
 
         # Testing
         sheet = wb["Sheet"]
-        sheet["D5"] = "I'm cell D5"
 
         # Get the encoding
         base = self.GetEncodingBase()
-        resample = (self.GetScaleX(None), self.GetScaleY(None))
-        img = self.im1.resize(resample)
         encoding = ImgToMonomer(img, n_monomers=base, print_data=False)
 
         # Get the raw pixel data for coloring
@@ -178,42 +142,6 @@ class application:
 
         wb.save(f)
 
-
-    def resize(self, event):
-        '''
-        Resamples the image on the right
-        '''
-        self.resample = (self.GetScaleX(None), self.GetScaleY(None))
-
-        # If an image hasn't been selected, do nothing
-        if self.raw_image == None:
-            return 0
-
-        # Get the PIL image 
-        pixel_art_pil = self.GetPixelArtDisplayImage(self.im1, self.resample, (1600,2400))
-        self.im2 = pixel_art_pil.resize((self.im1.width, self.im1.height))
-        self._DrawImages(None)
-
-    def set_size(self, event, size: tuple = (5,5)):
-        '''
-        Sets the size of the sliders to specific values
-        '''
-        self.scale_x.set(size[0])
-        self.scale_y.set(size[1])
-        self.resize(None)
-
-    def GetScaleX(self, event):
-        return self.scale_x.get()
-    
-    def GetScaleY(self, event):
-        return self.scale_y.get()
-
-    def GetContrast(self, event):
-        return self.contrast_slider.get()
-
-    def GetColor(self, event):
-        return self.color_slider.get()
-
     def _ChangeColorDepth(self, string_variable):
         '''
         Changes the color depth of the images.
@@ -222,44 +150,18 @@ class application:
         if debug: print(string_variable.get())
         sv = int(''.join([char for char in string_variable.get() if str(char).isnumeric()]))
 
-        # Set the original image to the 
+        # Set the original image to the reduced color depth
         self.im1 = ReduceBitsPerColorChannel(self.raw_scaled, sv)
+        self.image_to_save = ReduceBitsPerColorChannel(self.raw_image, sv)
             
         self.UpdatePixelatedImage(event=None)
         self._DrawImages(event=None)
 
-
-    def UpdateImageEnhancements(self, event):
-        contrast = self.GetContrast(None)
-        saturation = self.GetColor(None)
-
-        if self.raw_scaled == None:
-            return 0
-
-        # Get new image by applying enhancements to the raw image that is scaled
-        edited = self.raw_scaled
-        edited = ImageEnhance.Contrast(edited).enhance(contrast)
-        edited = ImageEnhance.Color(edited).enhance(saturation)
-        self.im1 = edited
-        self.UpdatePixelatedImage(event=None)
-
     def UpdatePixelatedImage(self, event):
         pixel_art_pil = self.GetPixelArtDisplayImage(self.im1, self.resample, (1600,2400))
-        self.im2 = pixel_art_pil.resize((self.im1.width, self.im1.height))
 
         #self.UpdateImageEnhancements(None)
         self._DrawImages(None)
-
-    def GetPixelArtDisplayImage(self,
-        img: Image, 
-        i_size: tuple, 
-        o_size: tuple):
-        '''
-        Takes an image and resamples it twice to get a pixelated
-        version of the original image with the same dimensions.
-        '''
-        small_img = img.resize(i_size, Image.BILINEAR)
-        return small_img.resize(img.size, Image.NEAREST)
 
     def ExitApplication(self):
         MsgBox = tk.messagebox.askquestion ('Exit Application','Are you sure you want to exit the application', icon = 'warning')
@@ -280,6 +182,7 @@ class application:
 
         # Raw image for whatever purpose
         self.raw_image = Image.open(f)
+        self.image_to_save = self.raw_image.copy()
 
         # Current size of the image
         size = (im1.width, im1.height)
@@ -295,14 +198,8 @@ class application:
         self.im1 = im1.resize(size, resample=Image.NEAREST)
         # Raw_scaled
         self.raw_scaled = im1.resize(size, resample = Image.NEAREST)
-        # Get the pixel art
-        self.resample = (self.GetScaleX(None), self.GetScaleY(None))
-        pixel_art_pil = self.GetPixelArtDisplayImage(im1, self.resample, (1600,2400))
-        # Set the pixel art to the appropriate size
-        self.im2 = pixel_art_pil.resize(size)
-
+    
         self._DrawImages(None)
-        self.UpdateImageEnhancements(None)
 
     def _DrawImages(self, event):
         '''
@@ -310,9 +207,7 @@ class application:
         self.im1 or self.im2
         '''
         self.tk_im1 = ImageTk.PhotoImage(self.im1)
-        self.tk_im2 = ImageTk.PhotoImage(self.im2)
         self.left_im.config(image=self.tk_im1)
-        self.right_im.config(image=self.tk_im2)
 
     def GetEncodingBase(self) -> int:
         base = self.bases.get()
@@ -337,8 +232,7 @@ class application:
         if self.im1 == None:
             MsgBox = tk.messagebox.showerror('Could not print RBG','You must have an active image\nto print the RGB values', icon = 'warning')
             return 0
-        resample = (self.GetScaleX(None), self.GetScaleY(None))
-        img = self.im1.resize(resample)
+        img = self.image_to_save
         
         self.output_text.delete('1.0', END)
 
@@ -346,7 +240,7 @@ class application:
 
         encodings = ImgToMonomer(img, n_monomers=base)
 
-        print(encodings)
+        pprint(encodings)
 
     def SaveImage(self):
         if self.im1 == None:
